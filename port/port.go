9820ac654d5d2c1e629521cb366485047011b2bd
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-09-06 00:13:32 +0700

    Implement sync.Mutex  and sync.Waitgroup

--- port/port.go
+++ port/port.go
@@ -3,50 +3,65 @@ package port
 import (
 	"net"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/rakarmp/athena/handler"
 )
 
-func ScanPort(protocol, hostname string, port int) handler.ScanResult {
+func ScanPort(protocol, hostname string, port int, wg *sync.WaitGroup, mutex *sync.Mutex, results *[]handler.ScanResult) {
+	defer wg.Done()
+
 	result := handler.ScanResult{Port: port}
 	address := hostname + ":" + strconv.Itoa(port)
 	conn, err := net.DialTimeout(protocol, address, 60*time.Second)
 
 	if err != nil {
 		result.State = "Closed"
-		return result
+	} else {
+		defer conn.Close()
+		result.State = "Open"
 	}
 
-	defer conn.Close()
-	result.State = "Open"
-	return result
+	mutex.Lock()
+	*results = append(*results, result)
+	mutex.Unlock()
 }
 
 func InitialScan(hostname string) []handler.ScanResult {
-	var result []handler.ScanResult
+	var results []handler.ScanResult
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
 
 	for i := 0; i <= 1024; i++ {
-		result = append(result, ScanPort("udp", hostname, i))
+		wg.Add(1)
+		go ScanPort("udp", hostname, i, &wg, &mutex, &results)
 	}
 
 	for i := 0; i <= 1024; i++ {
-		result = append(result, ScanPort("tcp", hostname, i))
+		wg.Add(1)
+		go ScanPort("tcp", hostname, i, &wg, &mutex, &results)
 	}
-	return result
+
+	wg.Wait()
+	return results
 }
 
 func WideScan(hostname string) []handler.ScanResult {
 	var results []handler.ScanResult
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
 
 	for i := 0; i <= 49152; i++ {
-		results = append(results, ScanPort("udp", hostname, i))
+		wg.Add(1)
+		go ScanPort("udp", hostname, i, &wg, &mutex, &results)
 	}
 
 	for i := 0; i <= 49152; i++ {
-		results = append(results, ScanPort("tcp", hostname, i))
+		wg.Add(1)
+		go ScanPort("tcp", hostname, i, &wg, &mutex, &results)
 	}
 
+	wg.Wait()
 	return results
-
 }
